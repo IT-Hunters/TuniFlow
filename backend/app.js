fcf969bea79c4c5175433b66bde650d45a16ed08
assilhammami <assil.hammami@esprit.tn>
2025-02-22 02:27:58 +0100

    Download CORS

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,6 +19,14 @@ mongo.connect(connection.url).then(()=>{
 }).catch(()=>{
   console.log("error connecting to db")
 });
+
+// Active CORS pour le frontend React (http://localhost:5173)
+app.use(cors({
+  origin: 'http://localhost:5173', // Remplacez par l'URL de votre frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
