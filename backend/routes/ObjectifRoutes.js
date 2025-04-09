88a03a460cad5c1735d0292ef991213f3d4c8e4f
dhiab <dhia.borji2001@gmail.com>
2025-04-09 14:00:07 +0100

    objectif fix

--- backend/routes/ObjectifRoutes.js
+++ backend/routes/ObjectifRoutes.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const { authenticateJWT } = require("../config/autorisation");
-const { createObjectif,findObjectifs,markObjectifAsCompleted,markObjectifAsFailed,deleteObjectifById,updateObjectifById
-    ,getAllObjectifsByProjectId,updateProgress,generateObjectiveReport,getObjectifTypes,getProjectsOverview,getbyid } = require("../controllers/ObjectifController");
-    const { getAllTypeObjectif } = require('../model/Objectif');
-router.post("/createobjectifs",createObjectif);
+const { createObjectif, findObjectifs, markObjectifAsCompleted, markObjectifAsFailed, deleteObjectifById, updateObjectifById,
+    getAllObjectifsByProjectId, updateProgress, generateObjectiveReport, getObjectifTypes, getProjectsOverview, getbyid } = require("../controllers/ObjectifController");
+
+router.post("/createobjectifs", authenticateJWT, createObjectif);
 router.get("/findobjectifs", authenticateJWT, findObjectifs);
 router.put("/complete/:objectifId", authenticateJWT, markObjectifAsCompleted);
 router.put("/fail/:objectifId", authenticateJWT, markObjectifAsFailed);
 router.delete("/deletobjectif/:objectifId", authenticateJWT, deleteObjectifById);
 router.put("/updateobjectif/:objectifId", authenticateJWT, updateObjectifById);
 router.get("/getAllObjectifsByProjectId/:projectId", authenticateJWT, getAllObjectifsByProjectId);
-router.get("/getbyid/:id",  getbyid);
-router.put('/updateProgress/:objectifId', authenticateJWT,updateProgress);
-router.get('/generateObjectiveReport/:projectId', generateObjectiveReport);
-router.get('/typesobjectif', getObjectifTypes);
-router.get('/getProjectsOverview/:projectSearch', getProjectsOverview);
+router.get("/getbyid/:id", authenticateJWT, getbyid); // Add authenticateJWT
+router.put('/updateProgress/:objectifId', authenticateJWT, updateProgress);
+router.get('/generateObjectiveReport/:projectId', authenticateJWT, generateObjectiveReport); // Add authenticateJWT
+router.get('/typesobjectif', getObjectifTypes); // No authentication needed
+router.get('/getProjectsOverview/:projectSearch', authenticateJWT, getProjectsOverview); // Add authenticateJWT
 
 module.exports = router;
