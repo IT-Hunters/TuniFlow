47cdcf3c7eec8f3bdfc7ab1ecca2de7e32c3b540
Youssef <yousseb.benarous@esprit.tn>
2025-05-11 19:52:53 +0000

    Deployement Config

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ mongoose.connect(connection.url)
 
 // 🟢 Enable CORS
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5000'],
+  origin: ['http://localhost:5173', 'http://localhost:5000',"*"],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control'], 
 }));
@@ -75,7 +75,7 @@ const server = http.createServer(app);
 
 global.io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5000",, 'https://tuniflow.netlify.app/'],
+    origin: ["http://localhost:5173", "http://localhost:5000","*", 'https://tuniflow.netlify.app/'],
     methods: ["GET", "POST"],
   },
 });
