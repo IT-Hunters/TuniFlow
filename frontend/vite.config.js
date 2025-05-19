312a74752405113e82a12cd4789088f0b5bbf647
wassimm1 <wassim.trabelsi@esprit.tn>
2025-05-20 00:18:57 +0200

    fix workflows

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
-import { fileURLToPath } from "url";
-import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
 
-  ],
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-      
+      '@': path.resolve(__dirname, 'src'),
     },
   },
-})
+  build: {
+    // Activer ou désactiver les sourcemaps selon vos besoins
+    sourcemap: true, // Changez à false si vous voulez désactiver pour le prod
+    // Optimisation des chunks
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Sépare les dépendances dans un chunk 'vendor'
+          }
+        },
+      },
+    },
+    // Ajuster la limite d'avertissement de taille des chunks
+    chunkSizeWarningLimit: 1000, // Augmente à 1000 kB pour réduire les warnings
+  },
+  // Configuration des assets pour résoudre les images comme darkBG.png
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
+});
